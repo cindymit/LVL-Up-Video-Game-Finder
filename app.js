3cb6af2e5aafbabe4a9e4f57416a5dfe857b511b
Cindy Mit <cindymit@Cindys-MacBook-Air.local>
2021-04-19 15:29:13 -0400

    Added pseudocode to js

---
+++ app.js
@@ -0,0 +1,52 @@
+const DOMAIN = 'https://api.rawg.io/api/';
+const API_KEY = '6759a297484a4e7dbebc9d3a8bb77ed4';
+const BASE_URL = `${DOMAIN}games?key={API_KEY}&`;
+// search url = `${DOMAIN}games?key={API_KEY}&search=${textInput}`;
+
+
+
+// Add HTML and connect all files.
+// Completed.
+
+
+
+// API request for search results.
+
+
+
+// Create event handler to log search bar text input.
+
+
+
+// Render search data. Create dynamic HTML.
+
+
+
+// Append search data.
+
+
+
+// Render image data.
+
+
+
+// Append image data.
+
+
+
+// Button event handler.
+
+
+
+// Remove current search.
+
+
+
+// Responsive Design.
+
+
+
+/* -----------POST-MVP----------- */
+
+// Search by platform.
+
